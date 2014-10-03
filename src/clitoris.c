feeb97569aef36993b1d865963e1521c104d6307
Sebastian Freundt <freundt@ga-group.nl>
2014-10-03 14:01:39 +0000

    minor, keep the return code of the underlying test run instead ...
    of clamping it to 1.

--- src/clitoris.c
+++ src/clitoris.c
@@ -1115,7 +1115,8 @@ run_tst(struct clit_chld_s ctx[static 1], struct clit_tst_s tst[static 1])
 		} else if (tst->exp_ret == 255U && rc) {
 			rc = 0;
 		} else {
-			rc = 1;
+			/* keep the test's rc */
+			rc = rc;
 		}
 	} else {
 		rc = 1;
